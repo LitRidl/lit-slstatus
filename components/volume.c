815cebc793a41b624eb229ec04d7ffc13d83e0bb
Michael Ridley <litridl@gmail.com>
2024-01-27 20:01:28 +0600

    Volume supporting PulseAudio/PipeWire

--- components/volume.c
+++ components/volume.c
@@ -1,219 +1,59 @@
-/* See LICENSE file for copyright and license details. */
-#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-#include "../slstatus.h"
-#include "../util.h"
+#include <pulse/pulseaudio.h>
 
-#if defined(__OpenBSD__) | defined(__FreeBSD__)
-	#include <poll.h>
-	#include <sndio.h>
-	#include <stdlib.h>
-	#include <sys/queue.h>
+static pa_mainloop *mainloop = NULL;
+static char vol_str[8] = {'\0'};
+static int volume_ready = 0;
 
-	struct control {
-		LIST_ENTRY(control)	next;
-		unsigned int		addr;
-	#define CTRL_NONE	0
-	#define CTRL_LEVEL	1
-	#define CTRL_MUTE	2
-		unsigned int		type;
-		unsigned int		maxval;
-		unsigned int		val;
-	};
+void context_state_cb(pa_context *c, void *userdata);
+void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata);
 
-	static LIST_HEAD(, control) controls = LIST_HEAD_INITIALIZER(controls);
-	static struct pollfd *pfds;
-	static struct sioctl_hdl *hdl;
-	static int initialized;
+const char *vol_perc(const char *card) {
+  volume_ready = 0;
 
-	/*
-	 * Call-back to obtain the description of all audio controls.
-	 */
-	static void
-	ondesc(void *unused, struct sioctl_desc *desc, int val)
-	{
-		struct control *c, *ctmp;
-		unsigned int type = CTRL_NONE;
+  mainloop = pa_mainloop_new();
+  pa_mainloop_api *mainloop_api = pa_mainloop_get_api(mainloop);
+  pa_context *context = pa_context_new(mainloop_api, "get_default_sink_volume");
 
-		if (desc == NULL)
-			return;
+  pa_context_set_state_callback(context, context_state_cb, (void *)card);
+  pa_context_connect(context, NULL, PA_CONTEXT_NOFLAGS, NULL);
 
-		/* Delete existing audio control with the same address. */
-		LIST_FOREACH_SAFE(c, &controls, next, ctmp) {
-			if (desc->addr == c->addr) {
-				LIST_REMOVE(c, next);
-				free(c);
-				break;
-			}
-		}
+  while (!volume_ready) {
+    pa_mainloop_iterate(mainloop, 1, NULL);
+  }
 
-		/* Only match output.level and output.mute audio controls. */
-		if (desc->group[0] != 0 ||
-		    strcmp(desc->node0.name, "output") != 0)
-			return;
-		if (desc->type == SIOCTL_NUM &&
-		    strcmp(desc->func, "level") == 0)
-			type = CTRL_LEVEL;
-		else if (desc->type == SIOCTL_SW &&
-			 strcmp(desc->func, "mute") == 0)
-			type = CTRL_MUTE;
-		else
-			return;
+  pa_context_disconnect(context);
+  pa_context_unref(context);
+  pa_mainloop_free(mainloop);
 
-		c = malloc(sizeof(struct control));
-		if (c == NULL) {
-			warn("sndio: failed to allocate audio control\n");
-			return;
-		}
+  return vol_str;
+}
 
-		c->addr = desc->addr;
-		c->type = type;
-		c->maxval = desc->maxval;
-		c->val = val;
-		LIST_INSERT_HEAD(&controls, c, next);
-	}
+void context_state_cb(pa_context *c, void *userdata) {
+  const char *card_name = userdata;
+  switch (pa_context_get_state(c)) {
+    case PA_CONTEXT_READY:
+      pa_context_get_sink_info_by_name(c, card_name, sink_info_cb, NULL);
+      break;
+    case PA_CONTEXT_FAILED:
+    case PA_CONTEXT_TERMINATED:
+      volume_ready = 1;
+      break;
+    default:
+      break;
+  }
+}
 
-	/*
-	 * Call-back invoked whenever an audio control changes.
-	 */
-	static void
-	onval(void *unused, unsigned int addr, unsigned int val)
-	{
-		struct control *c;
+void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
+  if (eol > 0) {
+    volume_ready = 1;
+    return;
+  }
 
-		LIST_FOREACH(c, &controls, next) {
-			if (c->addr == addr)
-				break;
-		}
-		c->val = val;
-	}
+  unsigned volume_percent = (unsigned)(100 * pa_cvolume_avg(&(i->volume)) / PA_VOLUME_NORM);
+  snprintf(vol_str, sizeof(vol_str), "%u", volume_percent);
 
-	static void
-	cleanup(void)
-	{
-		struct control *c;
-
-		if (hdl) {
-			sioctl_close(hdl);
-			hdl = NULL;
-		}
-
-		free(pfds);
-		pfds = NULL;
-
-		while (!LIST_EMPTY(&controls)) {
-			c = LIST_FIRST(&controls);
-			LIST_REMOVE(c, next);
-			free(c);
-		}
-	}
-
-	static int
-	init(void)
-	{
-		hdl = sioctl_open(SIO_DEVANY, SIOCTL_READ, 0);
-		if (hdl == NULL) {
-			warn("sndio: cannot open device");
-			goto failed;
-		}
-
-		if (!sioctl_ondesc(hdl, ondesc, NULL)) {
-			warn("sndio: cannot set control description call-back");
-			goto failed;
-		}
-
-		if (!sioctl_onval(hdl, onval, NULL)) {
-			warn("sndio: cannot set control values call-back");
-			goto failed;
-		}
-
-		pfds = calloc(sioctl_nfds(hdl), sizeof(struct pollfd));
-		if (pfds == NULL) {
-			warn("sndio: cannot allocate pollfd structures");
-			goto failed;
-		}
-
-		return 1;
-	failed:
-		cleanup();
-		return 0;
-	}
-
-	const char *
-	vol_perc(const char *unused)
-	{
-		struct control *c;
-		int n, v, value;
-
-		if (!initialized)
-			initialized = init();
-
-		if (hdl == NULL)
-			return NULL;
-
-		n = sioctl_pollfd(hdl, pfds, POLLIN);
-		if (n > 0) {
-			n = poll(pfds, n, 0);
-			if (n > 0) {
-				if (sioctl_revents(hdl, pfds) & POLLHUP) {
-					warn("sndio: disconnected");
-					cleanup();
-					initialized = 0;
-					return NULL;
-				}
-			}
-		}
-
-		value = 100;
-		LIST_FOREACH(c, &controls, next) {
-			if (c->type == CTRL_MUTE && c->val == 1)
-				value = 0;
-			else if (c->type == CTRL_LEVEL) {
-				v = (c->val * 100 + c->maxval / 2) / c->maxval;
-				/* For multiple channels return the minimum. */
-				if (v < value)
-					value = v;
-			}
-		}
-
-		return bprintf("%d", value);
-	}
-#else
-	#include <sys/soundcard.h>
-
-	const char *
-	vol_perc(const char *card)
-	{
-		size_t i;
-		int v, afd, devmask;
-		char *vnames[] = SOUND_DEVICE_NAMES;
-
-		if ((afd = open(card, O_RDONLY | O_NONBLOCK)) < 0) {
-			warn("open '%s':", card);
-			return NULL;
-		}
-
-		if (ioctl(afd, (int)SOUND_MIXER_READ_DEVMASK, &devmask) < 0) {
-			warn("ioctl 'SOUND_MIXER_READ_DEVMASK':");
-			close(afd);
-			return NULL;
-		}
-		for (i = 0; i < LEN(vnames); i++) {
-			if (devmask & (1 << i) && !strcmp("vol", vnames[i])) {
-				if (ioctl(afd, MIXER_READ(i), &v) < 0) {
-					warn("ioctl 'MIXER_READ(%ld)':", i);
-					close(afd);
-					return NULL;
-				}
-			}
-		}
-
-		close(afd);
-
-		return bprintf("%d", v & 0xff);
-	}
-#endif
+  volume_ready = 1;
+}
